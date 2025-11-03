e5b55a1bdb321133658e0c1872a0ddfeed2a0432
kenzy991 <145780027+kenzy991@users.noreply.github.com>
2025-11-03 16:59:33 +0200

    Improve Semaphore class implementation
    Refactor Semaphore class methods for clarity and error handling.

--- src/App.java
+++ src/App.java
@@ -2,18 +2,29 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+
 //Temp
 class Semaphore {
     private int value;
-    public Semaphore(int value) { this.value = value; }
-    public synchronized void waitSem() throws InterruptedException {
-        while (value == 0) wait();
-        value--;
+    public Semaphore(int value) {
+    this.value = value;
+}
+
+public synchronized void waitSemaphore() {
+    while (value == 0) {
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-    public synchronized void signal() {
-        value++;
-        notify();
-    }
+    value--;
+}
+public synchronized void signal() {
+    value++;    
+    notify();     
+}
+
 }
 
 //Temp
